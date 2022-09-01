c4722a4eda0b6212a96ec6896825c8f679c9e4d5
Shubham <shubmnagrare@gmail.com>
2022-09-01 23:54:12 +0530

    initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7b/5070fec76c13001d1279fb9582f3a57c
@@ -0,0 +1,14 @@
+package Misc;
+
+public class Reverse_Array {
+	
+	public void reverse() {
+		
+		int[] arr = {5,4,3,2,1};
+		
+		for(int i= arr.length -1; i >= 0;   i--) {
+			System.out.print("-- > " + arr[i]);
+		}
+	}
+
+}
---
+++ DataStructures/src/Misc/MaxHeap_UsingArray.java
@@ -0,0 +1,54 @@
+package Misc;
+
+public class MaxHeap_UsingArray {
+	
+	int theHeap[];
+	int capacity;
+	int pos;
+	
+	public MaxHeap_UsingArray() {
+		
+		pos = 1;
+		capacity = 10;
+		theHeap = new int[capacity];
+	}
+	
+
+	
+	public void print() {
+		
+		for(int i=1; i<pos; i++) {
+			System.out.println("--> " + theHeap[i]);
+		}
+	}
+	
+	public void insert(int value) {
+		
+		if(pos == capacity) {
+			return;
+		}
+		else {
+			
+			theHeap[pos++] = value;
+			
+			int child = pos-1;
+			int parent = child/2;
+			
+			while(theHeap[parent] < theHeap[child] && parent > 0) {
+				
+				int tmp = theHeap[parent];
+				theHeap[parent] = theHeap[child];
+				theHeap[child] = tmp;
+				
+				child = parent;
+				parent = child/2;
+			}
+			
+		}
+		
+	}
+	
+	
+	
+
+}
